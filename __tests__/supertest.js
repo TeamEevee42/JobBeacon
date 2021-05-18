6a6a53b0649b0b0f134ceff3c41cf1994056490e
Tobey Forsman <tobeyforsman@gmail.com>
2021-05-18 12:20:23 -0700

    Setup basic Express server

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -4,7 +4,7 @@ const request = require('supertest');
 const server = 'http://localhost:3000';
 
 describe('Testing Routes', () => {
-  describe('/jobs', () => {
+  describe('/job', () => {
     describe('GET', () => {
       xit('responds with 200 status and text/html content type', () => request(server)
         .get('/')
