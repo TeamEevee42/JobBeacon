38069dc65094abca02901184bd7d8e2dfc960824
Joe Bigelow <joebigelow@protonmail.com>
2021-05-18 22:02:27 -0500

    Begin connecting database
    Co-authored-by: Jorge Carlos Fernandez <jcferna1@gmail.com>
    Co-authored-by: Tobey Forsman <116612+yebot@users.noreply.github.com>
    Co-authored-by: Mo Hmaidi <mhmaidi789@gmail.com>

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,7 +1,15 @@
+const db = require('../models/jobModels.js');
+
 const jobController = {};
 
 jobController.getJobs = (req, res, next) => {
-  next();
+  co
+  db.query('SELECT * FROM job_posting')
+    .then(((results) => {
+      console.log(results);
+      next();
+    }
+    ));
 };
 
 module.exports = jobController;
